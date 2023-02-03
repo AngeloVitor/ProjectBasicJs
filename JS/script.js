101823c1d98d6cdd5d66cdff82423b32c0f2e9de
Ângelo Lemaire <angelovitor2010@gmail.com>
2023-02-03 00:06:13 -0300

    modules

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+import initTabNav from "./modules/tabnav.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAccordion  from "./modules/accordion.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+
+
+initTabNav();
+initScrollSuave();
+initAccordion();
+initAnimacaoScroll();
