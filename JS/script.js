5dc3f6b49ec7ad62a182491c9b66635a355abc1d
Ângelo Lemaire <angelovitor2010@gmail.com>
2023-02-06 23:29:22 -0300

    ToolTip and DropDown

--- JS/script.js
+++ JS/script.js
@@ -2,9 +2,17 @@ import initTabNav from "./modules/tabnav.js";
 import initScrollSuave from "./modules/scroll-suave.js";
 import initAccordion  from "./modules/accordion.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initTooltip from "./modules/tooltip.js";
+import outsideClick from "./modules/outsideclick.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initModal from "./modules/modal.js";
 
 
 initTabNav();
 initScrollSuave();
 initAccordion();
 initAnimacaoScroll();
+initTooltip();
+outsideClick();
+initDropdownMenu();
+initModal();
